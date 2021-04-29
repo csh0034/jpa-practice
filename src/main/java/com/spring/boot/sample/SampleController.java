d131fc0bc253c83a7abf72a692cbaf9bdbe93843
csh0034 <csh0034@gmail.com>
2021-04-29 18:32:04 +0900

    lol api test, RestTemplate bean 등록

--- src/main/java/com/spring/boot/sample/SampleController.java
+++ src/main/java/com/spring/boot/sample/SampleController.java
@@ -3,29 +3,20 @@ package com.spring.boot.sample;
 import com.spring.boot.sample.validator.SampleValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StreamUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,35 +26,9 @@ import java.util.Map;
 public class SampleController {
 
     private final SampleService sampleService;
-    private final RestTemplateBuilder restTemplateBuilder;
+    private final RestTemplate restTemplate;
     private final SampleValidator sampleValidator;
     private final HttpSession httpSession;
-
-    private RestTemplate restTemplate() {
-        return restTemplateBuilder
-                .setReadTimeout(Duration.ofMillis(5000))
-                .setConnectTimeout(Duration.ofMillis(5000))
-                .additionalInterceptors((request, body, execution) -> {
-
-                    URI uri = request.getURI();
-
-                    StringBuilder reqLog = new StringBuilder();
-                    reqLog.append("[REQ] ").append("Uri : ").append(request.getURI()).append(", Method : ").append(request.getMethod()).append(", Request Body : ")
-                            .append(new String(body, StandardCharsets.UTF_8));
-                    System.out.println(reqLog.toString());
-
-                    ClientHttpResponse response = execution.execute(request, body);
-
-                    StringBuilder resLog = new StringBuilder();
-                    resLog.append("[RES] ").append("Uri : ").append(uri).append(", Status code : ").append(response.getStatusCode()).append(", Response Body : ")
-                            .append(StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8));
-                    System.out.println(resLog.toString());
-
-                    return response;
-                })
-                .requestFactory(() -> new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory()))
-                .build();
-    }
 
     @RequestMapping("/sample")
     public String sample(@RequestParam Map<String, String> param) {
@@ -84,7 +49,7 @@ public class SampleController {
         //parameters.put("test2", "test2");
 
         try {
-            returnValue = restTemplate().postForObject("sadf", parameters, String.class);
+            returnValue = restTemplate.postForObject("sadf", parameters, String.class);
         } catch (Exception e) {
             System.out.println(e.toString());
             e.printStackTrace();
