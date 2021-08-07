1ba6bd38b35df262f4836994109b79b4be0f96a0
csh0034 <csh0034@gmail.com>
2021-08-07 20:10:35 +0900

    스프링 데이터 JPA 강의 - Auditing

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,8 +1,14 @@
 package study.datajpa;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +16,9 @@ public class DataJpaApplication {
         SpringApplication.run(DataJpaApplication.class, args);
     }
 
+    // 실제론 시큐리티 컨텍스트 또는 세션에서 인증정보 가져와서 등록되게 해야함
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,47 +1,74 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.List;
+import study.datajpa.repository.MemberRepository;
 
 @SpringBootTest
+@Transactional
 public class MemberTest {
 
-    @PersistenceContext
-    EntityManager em;
+  @PersistenceContext
+  EntityManager em;
 
-    @Test
-    @Transactional
-    public void testEntity() {
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
+  @Autowired
+  MemberRepository memberRepository;
 
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
+  @Test
+  public void testEntity() {
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
 
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
 
-        //초기화
-        em.flush();
-        em.clear();
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
 
-        //확인
-        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+    //초기화
+    em.flush();
+    em.clear();
 
-        for (Member member : members) {
-            System.out.println("member=" + member);
-            System.out.println("-> member.team=" + member.getTeam());
-        }
+    //확인
+    List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+    for (Member member : members) {
+      System.out.println("member=" + member);
+      System.out.println("-> member.team=" + member.getTeam());
     }
+  }
+
+  @Test
+  public void JpaEventBaseEntity() throws Exception {
+    //given
+    Member member = new Member("member1");
+    memberRepository.save(member);
+
+    Thread.sleep(100);
+    member.setUsername("member2");
+
+    em.flush();
+    em.clear();
+
+    //when
+    Member findMember = memberRepository.findById(member.getId()).get();
+
+    //then
+    System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+    System.out.println("findMember.getLastModifiedDate = " + findMember.getLastModifiedDate());
+    System.out.println("findMember.getCreatedBy = " + findMember.getCreatedBy());
+    System.out.println("findMember.getLastModifiedBy = " + findMember.getLastModifiedBy());
+  }
 }
